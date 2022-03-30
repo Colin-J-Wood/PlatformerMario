0791af7d9f1cfc5ce247555f46c5310c9955407d
Kaleidio <w012764k@student.staffs.ac.uk>
2022-03-30 15:02:39 +0100

    make the hardest jump easier
    also sped up the koopas for better challenge
    also changed the timings on koopas.  note that for whatever reason, all timers will always be half the expected timing.

--- PlatformerMario/PlatformerMario/Constants.h
+++ PlatformerMario/PlatformerMario/Constants.h
@@ -5,7 +5,7 @@
 #define SCREEN_HEIGHT 407
 #define TARGET_MILLISEC 4
 
-#define PLAYER_SPEED Vector2D(75.0f, 225.0f)
+#define PLAYER_SPEED Vector2D(85.0f, 225.0f)
 #define PLAYER_JUMP_TIME 0.5f
 #define PLAYER_COLLISION_RADIUS 15.0f
 #define PLAYER_DEATH_TIME 5.0f
@@ -17,5 +17,5 @@
 #define SHAKE_DURATION 0.5f
 
 #define KOOPA_INJURED_TIME 10.0f
-#define KOOPA_SPEED Vector2D(35.0f, 75.0f)
-#define KOOPA_RESPAWN_TIME 2.0f
+#define KOOPA_SPEED Vector2D(75.0f, 75.0f)
+#define KOOPA_RESPAWN_TIME 4.0f
